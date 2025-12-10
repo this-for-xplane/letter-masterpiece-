7b6454d5f94e91b21ad67766fb01bef771bb42c3
SeoJun <129959256+this-for-xplane@users.noreply.github.com>
2025-12-10 23:59:55 +0900

    Create fail.tsx

---
+++ src/pages/fail.tsx
@@ -0,0 +1,7 @@
+export default function Fail() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-500">
+      <div className="text-white text-4xl font-bold">결제가 취소되었습니다</div>
+    </div>
+  );
+}

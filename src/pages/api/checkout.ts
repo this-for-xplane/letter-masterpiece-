f1e66c9e2cc1317c930cc0701e9f13e8bcee72f2
SeoJun <this-for-xplane@users.noreply.github.com>
2025-12-12 00:21:10 +0900

    Add Stripe dependency
    Signed-off-by: SeoJun <this-for-xplane@users.noreply.github.com>

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2025-11-17.clover",
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
